44479622bdb6119c5009a94c06b28147d5d0bde3
bmiww <bmiww@bky.one>
2024-04-19 16:45:11 +0200

    Structure for tracking clients

--- bm-cl-wayland.lisp
+++ bm-cl-wayland.lisp
@@ -5,6 +5,7 @@
 ;; ██║███╗██║██╔══██║  ╚██╔╝  ██║     ██╔══██║██║╚██╗██║██║  ██║
 ;; ╚███╔███╔╝██║  ██║   ██║   ███████╗██║  ██║██║ ╚████║██████╔╝
 ;;  ╚══╝╚══╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝
+;; TODO: You might need to keep a list of all the interfaces that are actually globals.......
 ;; TODO: Maybe one day try to refer to the darn book: https://github.com/rcalixte/wayland-book
 ;; It is however also highly incomplete
 (defpackage #:bm-cl-wayland
@@ -13,18 +14,41 @@
   (:export display-create))
 (in-package :bm-cl-wayland)
 
-(defvar *client-tracker* (make-hash-table :test 'equal))
-
-(defclass client ()
-  ((objects :initform (make-hash-table :test 'equal) :accessor objects)))
-
 (defclass object ()
   ((display :initarg :display :reader display)
    (version :initarg :version :reader version)))
 
 (defclass global (object) ())
-(defgeneric create-global (global interface version))
 
+;; ┌─┐┬  ┬┌─┐┌┐┌┌┬┐
+;; │  │  │├┤ │││ │
+;; └─┘┴─┘┴└─┘┘└┘ ┴
+(defclass client ()
+  ((objects :initform (make-hash-table :test 'equal) :accessor objects)))
+
+(defvar *client-tracker* (make-hash-table :test 'equal))
+
+(defun client-get-credentials (client)
+  (with-foreign-objects ((pid :int) (uid :int) (gid :int))
+    (wl-ffi::client-get-credentials client pid uid gid)
+    (values (mem-aref pid :int) (mem-aref uid :int) (mem-aref gid :int))))
+
+(defun get-client (client-ptr)
+  (let* ((pid (client-get-credentials client))
+	 (client (gethash pid *client-tracker*)))
+    (unless client (error (format nil "No client found for pid ~a" pid)))
+    client))
+
+
+;; ┌┬┐┌─┐┌┬┐┌─┐
+;;  ││├─┤ │ ├─┤
+;; ─┴┘┴ ┴ ┴ ┴ ┴
+
+;; TODO: Need to create a way to insert incremental/random values here
+;; TODO: Might not need to be a hash-table since i intend this to be ephemeral
+(defvar *data-tracker* (make-hash-table :test 'equal))
+
+(defun get-data (data-ptr) (gethash (mem-ref data-ptr :int) *data-tracker*))
 
 ;; ┌─┐┌─┐┬  ┌─┐┬  ┌─┐┌─┐┌─┐
 ;; ├┤ ├┤ │  │  │  ├─┤└─┐└─┐
@@ -34,14 +58,12 @@
   (:nicknames :wl-compositor))
 (in-package #:bm-cl-wayland.compositor)
 
-(defclass compositor (global)
-  ((create-surface :initarg :create-surface :reader create-surface)
-   (create-region :initarg :create-region :reader create-region)
-   (bind :initarg :bind :reader bind))
+(defclass compositor (global) ()
   (:default-initargs :version 4))
 
-(defmethod create-global ((compositor compositor))
-  (global-create (display compositor) *interface* (version compositor) (null-pointer) ))
+(defgeneric create-surface (client resource id))
+(defgeneric create-region (client resource id))
+(defgeneric bind (client resource id))
 
 (defvar *bind* nil)
 (defvar *interface* nil)
@@ -49,22 +71,25 @@
   (create-surface :pointer)
   (create-region :pointer))
 
-(cl-async::define-c-callback bind :void ((client :pointer) (data :pointer) (version :uint) (id :uint))
-  (format t "BIND: ~a ~a ~a ~a~%" client data version id)
+(cl-async::define-c-callback bind-ffi :void ((client :pointer) (data :pointer) (version :uint) (id :uint))
+  (let* ((client (get-client client))
+	 (data (get-data data)))
+    (funcall (bind client data (mem-ref version :uint) (mem-ref id :uint)))))
 
-(cl-async::define-c-callback create-surface :void ((client :pointer) (resource :pointer) (id :uint))
+(cl-async::define-c-callback create-surface-ffi :void ((client :pointer) (resource :pointer) (id :uint))
   (format t "CREATE-SURFACE: ~a ~a ~a~%" client resource id))
 
-(cl-async::define-c-callback create-region :void ((client :pointer) (resource :pointer) (id :uint))
+(cl-async::define-c-callback create-region-ffi :void ((client :pointer) (resource :pointer) (id :uint))
   (format t "CREATE-REGION: ~a ~a ~a~%" client resource id))
 
 (defmethod initialize-instance :after ((compositor compositor) &key)
-  (unless *bind* (setf *bind* (callback bind)))
+  (unless *bind* (setf *bind* (callback bind-ffi)))
   (unless *interface*
     (with-foreign-object (interface 'interface)
-      (setf (foreign-slot-value interface '(:struct interface) 'create-surface) (callback create-surface))
-      (setf (foreign-slot-value interface '(:struct interface) 'create-region) (callback create-region))
-      (setf *interface* interface))))
+      (setf (foreign-slot-value interface '(:struct interface) 'create-surface) (callback create-surface-ffi))
+      (setf (foreign-slot-value interface '(:struct interface) 'create-region) (callback create-region-ffi))
+      (setf *interface* interface)))
+  (global-create (display compositor) *interface* (version compositor) (null-pointer) *bind*))
 
 
 ;; ┌─┐─┐ ┬┌┬┐┌─┐┌┐┌┌─┐┬┌─┐┌┐┌  ┌─┐┬  ┌─┐┌─┐┌─┐
@@ -75,7 +100,6 @@
 (defclass compositor (bm-cl-wayland.compositor::compositor)
   ())
 
-;; (defmethod initialize-instance ())
-
-(defmethod create-global ((global global) interface version)
-  (global-create (display global) ))
+(defun pretend-func ()
+  (let ((display (display-create)))
+    (let ((compositor (make-instance 'compositor :display display))))))
--- libwayland-ffi.lisp
+++ libwayland-ffi.lisp
@@ -7,6 +7,7 @@
 ;; ╚═╝     ╚═╝     ╚═╝
 (defpackage :bm-cl-libwayland
   (:use :cl :cffi)
+  (:nicknames :wl-ffi)
   (:export display-create global-create))
 
 (in-package :bm-cl-libwayland)
@@ -21,3 +22,9 @@
   (version :int32)
   (data :pointer)
   (func :pointer))
+
+(defcfun ("wl_client_get_credentials" client-get-credentials) :void
+  (client :pointer)
+  (pid :pointer)
+  (uid :pointer)
+  (gid :pointer))
